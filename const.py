6c9d20add0ac6bb7a36437896952628c17ac9939
Fetter <93355839+159753Fetter@users.noreply.github.com>
2025-03-22 15:25:16 +0800

    Update const.py

--- const.py
+++ const.py
@@ -1,8 +1,7 @@
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:wyq20040519@localhost:3306/software"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:xxxxx@localhost:3306/software"
 SQLALCHEMY_DATABASE_URL_MASTER = "mysql+pymysql://SDUBAS-admin:SDUBASmysql123!@localhost:3306/SDUBAS"
 SQLALCHEMY_DATABASE_URL_SLAVE = "mysql+pymysql://SDUBAS-admin:SDUBASmysql123!@localhost:3306/SDUBAS"
-# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://handsome_boy:SDUBASmysql123!@43.138.34.119:3306/"
 development_ip = '127.0.0.1'
-server_ip = "43.138.34.119"
-base_url = "http://172.16.2.6:8000"
-redis_password = '123456'
+server_ip = "xxxxxxxx"
+base_url = "http://xxxxxxx:8000"
+redis_password = 'xxxxxx'
